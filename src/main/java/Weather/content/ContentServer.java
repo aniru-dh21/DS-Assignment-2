f9e9ea2ea52e96f778c3e16651ecb88ec8bf9501
Ramachandra Anirudh Vemulapalli <vemulapallianirudh18@gmail.com>
2025-09-03 12:02:12 +0930

    JSON File reader

--- src/main/java/Weather/content/ContentServer.java
+++ src/main/java/Weather/content/ContentServer.java
@@ -2,6 +2,8 @@ package Weather.content;
 
 import Weather.util.LamportClock;
 
+import java.io.*;
+
 public class ContentServer {
     private final LamportClock clock = new LamportClock();
 
@@ -15,5 +17,23 @@ public class ContentServer {
         String filePath = args[1];
 
         ContentServer cs = new ContentServer();
+    }
+
+    private JSONObject readFile(String path) throws IOException {
+        JSONObject json = new JSONObject();
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(":", 2);
+                if (parts.length == 2) {
+                    json.put(parts[0], parts[1].trim());
+                }
+            }
+        }
+        return json;
+    }
+
+    private void sendPut(String serverAddr, JSONObject json) throws IOException {
+        // Todo: Building PUT HTTP Request and sending request via socket
     }
 }
